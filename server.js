da4ed83d45036ea444e867275ad41ebf1f628316
udisha <udisha@gmail.com>
2019-11-17 17:32:18 +0530

    Sample

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 4321
+
+app.get('/',(req,res)=>{
+    res.send('Hello! This is sample app')
+})
+
+app.get('/hi',(req,res)=>{
+    res.send('Hi! '+req.query.name)
+})
+
+app.get('/bye',(req,res)=>{
+    res.send('GoodBye! '+req.query.name)
+})
+
+
+app.listen(PORT,()=>{
+    console.log('Server started at http://localhost:4321 '+PORT)
+})
